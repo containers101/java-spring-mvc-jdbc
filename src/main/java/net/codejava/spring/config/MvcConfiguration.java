dd85c75cd9670a2793500830077f0f3a632c3229
nikolai <n.ivanov.p@gmail.com>
2017-01-27 11:17:38 +0300

    fixed mysql username/pwd

--- src/main/java/net/codejava/spring/config/MvcConfiguration.java
+++ src/main/java/net/codejava/spring/config/MvcConfiguration.java
@@ -38,7 +38,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
-		dataSource.setUsername("root");
+		dataSource.setUsername("admin");
 		dataSource.setPassword("admin");
 		
 		return dataSource;
