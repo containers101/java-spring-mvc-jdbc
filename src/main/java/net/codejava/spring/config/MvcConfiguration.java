0db6f123ceb94f10348af647b89235cd1e423258
nikolai <n.ivanov.p@gmail.com>
2017-01-27 12:14:37 +0300

    changed mysql host

--- src/main/java/net/codejava/spring/config/MvcConfiguration.java
+++ src/main/java/net/codejava/spring/config/MvcConfiguration.java
@@ -37,7 +37,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	public DataSource getDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+		dataSource.setUrl("jdbc:mysql://mysql/contactdb");
 		dataSource.setUsername("admin");
 		dataSource.setPassword("admin");
 		
