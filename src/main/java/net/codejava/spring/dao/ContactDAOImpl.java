d45a22dea6945dea15c73267b47e82310e319f38
nikolai <n.ivanov.p@gmail.com>
2017-01-27 11:59:32 +0300

    updated sql query

--- src/main/java/net/codejava/spring/dao/ContactDAOImpl.java
+++ src/main/java/net/codejava/spring/dao/ContactDAOImpl.java
@@ -24,6 +24,22 @@ public class ContactDAOImpl implements ContactDAO {
 	
 	public ContactDAOImpl(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
+		createDatabase();
+	}
+
+	private void createDatabase() {
+		String sql = "CREATE DATABASE IF NOT EXISTS contactdb;";
+		jdbcTemplate.execute(sql);
+
+		String sqlTable = "CREATE TABLE 'contact' (" +
+				"  'contact_id' int(11) NOT NULL AUTO_INCREMENT," +
+				"  'name' varchar(45) NOT NULL," +
+				"  'email' varchar(45) NOT NULL," +
+				"  'address' varchar(45) NOT NULL," +
+				"  'telephone' varchar(45) NOT NULL," +
+				"  PRIMARY KEY ('contact_id')" +
+				") ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;";
+		jdbcTemplate.execute(sqlTable);
 	}
 
 	@Override
