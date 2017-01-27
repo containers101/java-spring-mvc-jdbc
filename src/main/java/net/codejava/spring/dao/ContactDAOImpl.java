ff7bff4d9e7c6bef0b0b36bdc0d0651918b6c838
nikolai <n.ivanov.p@gmail.com>
2017-01-27 12:25:45 +0300

    changed mysql host

--- src/main/java/net/codejava/spring/dao/ContactDAOImpl.java
+++ src/main/java/net/codejava/spring/dao/ContactDAOImpl.java
@@ -31,13 +31,13 @@ public class ContactDAOImpl implements ContactDAO {
 		String sql = "CREATE DATABASE IF NOT EXISTS contactdb;";
 		jdbcTemplate.execute(sql);
 
-		String sqlTable = "CREATE TABLE 'contact' (" +
-				"  'contact_id' int(11) NOT NULL AUTO_INCREMENT," +
-				"  'name' varchar(45) NOT NULL," +
-				"  'email' varchar(45) NOT NULL," +
-				"  'address' varchar(45) NOT NULL," +
-				"  'telephone' varchar(45) NOT NULL," +
-				"  PRIMARY KEY ('contact_id')" +
+		String sqlTable = "CREATE TABLE `contact` (" +
+				"  `contact_id` int(11) NOT NULL AUTO_INCREMENT," +
+				"  `name` varchar(45) NOT NULL," +
+				"  `email` varchar(45) NOT NULL," +
+				"  `address` varchar(45) NOT NULL," +
+				"  `telephone` varchar(45) NOT NULL," +
+				"  PRIMARY KEY (`contact_id`)" +
 				") ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;";
 		jdbcTemplate.execute(sqlTable);
 	}
